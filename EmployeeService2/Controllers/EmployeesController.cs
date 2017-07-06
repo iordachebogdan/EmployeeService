e91aeaa6c3297dab1fe1d3fdd406ff5b5e03dd3a
Bogdan Iordache <iordache_bogdan1998@yahoo.com>
2017-07-06 14:46:19 +0300

    Added query string parameter for Gender in Get method

--- EmployeeService2/Controllers/EmployeesController.cs
+++ EmployeeService2/Controllers/EmployeesController.cs
@@ -10,11 +10,25 @@ namespace EmployeeService2.Controllers
 {
     public class EmployeesController : ApiController
     {
-        public IEnumerable<Employee> Get()
+        public HttpResponseMessage Get(string gender = "All")
         {
             using(EmployeeDBEntities entities = new EmployeeDBEntities())
             {
-                return entities.Employees.ToList();
+                switch (gender.ToLower())
+                {
+                    case "all":
+                        return Request.CreateResponse(HttpStatusCode.OK,
+                            entities.Employees.ToList());
+                    case "male":
+                        return Request.CreateResponse(HttpStatusCode.OK,
+                            entities.Employees.Where(e => e.Gender.ToLower() == "male").ToList());
+                    case "female":
+                        return Request.CreateResponse(HttpStatusCode.OK,
+                           entities.Employees.Where(e => e.Gender.ToLower() == "female").ToList());
+                    default:
+                        return Request.CreateErrorResponse(HttpStatusCode.BadRequest,
+                            "Value for gender must be Male, Female or All. " + gender + " is invalid.");
+                }
             }
         }
 
